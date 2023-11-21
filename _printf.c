b5ecbdb5ddd37cb20d18d333835cd1fd457c9e17
GoldenHatchet15 <raphsant20@gmail.com>
2023-11-21 13:32:46 -0400

    printf task.0

--- _printf.c
+++ _printf.c
@@ -1,24 +1,26 @@
-
 #include<stdarg.h>
 #include<stdio.h>
 #include"main.h"
 #include<unistd.h>
+
 
 int _printf(const char *format, ...)
 {
     va_list args;
     int count = 0;
 
-    if (!format)
-        return (-1);
-
     va_start(args, format);
 
     while (*format)
     {
-        if (*format == '%' && *(format + 1) != '\0')
+        if (*format == '%')
         {
             format++;
+            /* Check if the next character is the end of the string*/
+            if (*format == '\0')
+            {
+	      break; /* Exit the loop, do not print anything for standalone '%'*/
+            }
             switch (*format)
             {
                 case 'c':
@@ -26,23 +28,24 @@ int _printf(const char *format, ...)
                     break;
                 case 's':
                     {
-                        char *s = va_arg(args, char *);
-                        if (!s)
-                            s = "(null)";
-                        while (*s)
-                            count += _putchar(*s++);
+                        char *str = va_arg(args, char *);
+                        if (str == NULL)
+                            str = "(null)";
+                        while (*str)
+                            count += _putchar(*str++);
                     }
                     break;
                 case '%':
                     count += _putchar('%');
                     break;
                 default:
+		  /* Handle unknown format specifiers*/
                     count += _putchar('%');
                     count += _putchar(*format);
                     break;
             }
         }
-        else if (*format != '%')
+        else
         {
             count += _putchar(*format);
         }
