0c73911dd0da936cc3cf8a99be6f164b6e35afb7
GoldenHatchet15 <raphsant20@gmail.com>
2023-11-21 13:35:25 -0400

    printf task.0

--- _printf.c
+++ _printf.c
@@ -3,56 +3,64 @@
 #include"main.h"
 #include<unistd.h>
 
+/**
+*_printf-Customprintffunctiontooutputaccordingtoaformat.
+*@format:Formatstringcontainingspecifiers(%c,%s,%%).
+*Doesn'thandleflags,fieldwidth,precision,orlength.
+*Return:Numberofcharactersprinted(excludingthenullbyteused
+*toendoutputofstrings)
+*/
 
-int _printf(const char *format, ...)
+int _printf(const char *format,...)
 {
-    va_list args;
-    int count = 0;
+va_list args;
+int count=0;
 
-    va_start(args, format);
+if(!format)
+return(-1);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            /* Check if the next character is the end of the string*/
-            if (*format == '\0')
-            {
-	      break; /* Exit the loop, do not print anything for standalone '%'*/
-            }
-            switch (*format)
-            {
-                case 'c':
-                    count += _putchar(va_arg(args, int));
-                    break;
-                case 's':
-                    {
-                        char *str = va_arg(args, char *);
-                        if (str == NULL)
-                            str = "(null)";
-                        while (*str)
-                            count += _putchar(*str++);
-                    }
-                    break;
-                case '%':
-                    count += _putchar('%');
-                    break;
-                default:
-		  /* Handle unknown format specifiers*/
-                    count += _putchar('%');
-                    count += _putchar(*format);
-                    break;
-            }
-        }
-        else
-        {
-            count += _putchar(*format);
-        }
-        format++;
-    }
+va_start(args,format);
 
-    va_end(args);
+while(*format)
+{
+if(*format=='%')
+{
+format++;
+if(!*format)/*Endofstringafter'%'*/
+{
+break;/*Donotprintanythingandexit*/
+}
+switch(*format)
+{
+case'c':
+count+=_putchar(va_arg(args,int));
+break;
+case's':
+{
+char*s=va_arg(args,char*);
+if(!s)
+s="(null)";
+while(*s)
+count+=_putchar(*s++);
+break;
+}
+case'%':
+count+=_putchar('%');
+break;
+default:
+count+=_putchar('%');
+count+=_putchar(*format);
+break;
+}
+}
+else
+{
+count+=_putchar(*format);
+}
+format++;
+}
 
-    return (count);
+va_end(args);
+
+return(count);
 }
