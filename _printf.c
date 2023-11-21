de6b169a9166ce1217e5748ce8a3e11baab46d38
GoldenHatchet15 <raphsant20@gmail.com>
2023-11-21 13:27:08 -0400

    printf task.0

--- _printf.c
+++ _printf.c
@@ -1,66 +1,49 @@
-#include<stdarg.h>
-#include<stdio.h>
-#include"main.h"
-#include<unistd.h>
-
-/**
-*_printf-Customprintffunctiontooutputaccordingtoaformat.
-*@format:Formatstringcontainingspecifiers(%c,%s,%%).
-*Doesn'thandleflags,fieldwidth,precision,orlength.
-*Return:Numberofcharactersprinted(excludingthenullbyteused
-*toendoutputofstrings)
-*/
-
-int _printf(const char *format,...)
+int _printf(const char *format, ...)
 {
-va_list args;
-int count=0;
+    va_list args;
+    int count = 0;
 
-if(!format)
-return(-1);
+    if (!format)
+        return (-1);
 
-va_start(args,format);
+    va_start(args, format);
 
-while(*format)
-{
-if(*format=='%')
-{
-format++;
-if(!*format)/*Endofstringafter'%'*/
-{
-break;/*Donotprintanythingandexit*/
-}
-switch(*format)
-{
-case'c':
-count+=_putchar(va_arg(args,int));
-break;
-case's':
-{
-char*s=va_arg(args,char*);
-if(!s)
-s="(null)";
-while(*s)
-count+=_putchar(*s++);
-break;
-}
-case'%':
-count+=_putchar('%');
-break;
-default:
-count+=_putchar('%');
-count+=_putchar(*format);
-break;
-}
-}
-else
-{
-count+=_putchar(*format);
-}
-format++;
-}
+    while (*format)
+    {
+        if (*format == '%' && *(format + 1) != '\0')
+        {
+            format++;
+            switch (*format)
+            {
+                case 'c':
+                    count += _putchar(va_arg(args, int));
+                    break;
+                case 's':
+                    {
+                        char *s = va_arg(args, char *);
+                        if (!s)
+                            s = "(null)";
+                        while (*s)
+                            count += _putchar(*s++);
+                    }
+                    break;
+                case '%':
+                    count += _putchar('%');
+                    break;
+                default:
+                    count += _putchar('%');
+                    count += _putchar(*format);
+                    break;
+            }
+        }
+        else if (*format != '%')
+        {
+            count += _putchar(*format);
+        }
+        format++;
+    }
 
-va_end(args);
+    va_end(args);
 
-return(count);
+    return (count);
 }
