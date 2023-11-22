b5321f2f7c687d06937078a5d01670a31a11d9e5
Eidal559 <eidaljuarbe@gmail.com>
2023-11-22 05:04:19 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1, on error -1
- */
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1, on error -1
+*/
 int _putchar(char c)
 {
 return write(1, &c, 1);
