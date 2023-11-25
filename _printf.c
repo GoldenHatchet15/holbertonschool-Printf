b57493c889bc915607bb9be8a2849d1b70684fcc
GoldenHatchet15 <raphsant20@gmail.com>
2023-11-25 02:18:36 -0400

    printf task.0

--- _printf.c
+++ _printf.c
@@ -10,14 +10,6 @@
 * to end output of strings)
 */
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
-
-int _putchar(char c) {
-  return (write(1, &c, 1));
-}
-
 int print_char(va_list args) {
   return (_putchar(va_arg(args, int)));
 }
